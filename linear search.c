45f632a88b35dc5372ec7b0f9252a67c71356a7d
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-05-20 19:21:31 +0530

    Add files via upload

---
+++ linear search.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int fun(int *arr,int n,int s)
+{
+	int i,x=0;
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]==s)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+void main()
+{
+	int n,s,i;
+	scanf("%d",&n);
+	int arr[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	scanf("%d",&s);
+	if(fun(arr,n,s))
+	printf("True");
+	else
+	printf("False");
+}
